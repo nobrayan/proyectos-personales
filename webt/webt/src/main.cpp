17ac0ddd955352797f37ffabd1347f623fd83ba9
Brayan <bmalexisvn@gmail.com>
2025-03-11 21:38:35 -0500

    Add files via upload

--- webt/webt/src/main.cpp
+++ webt/webt/src/main.cpp
@@ -1,42 +1,57 @@
+/*
+
+Creado por @nobrayan
+WeBT (Bluetooth Classic)
+en conjunto con webt 1.0.0-beta.apk (o superior)
+
+*/
+
 #include <Arduino.h>
 #include <Preferences.h>
 #include <BluetoothSerial.h>
 #include <WiFi.h>
-#include <ESPmDNS.h>
 #include <WebServer.h>
 
-const String btname = "WeBT"; // Nombre del dispositivo Bluetooth
-const String mdnsname = "esp32"; // Nombre de la red mDNS (http://esp32.local)
+// [WeBT] es el prefijo que se mostrara en el nombre del dispositivo
+// Si el prefijo se cambia, tambien se debe cambiar en la aplicacion movil
+const String prefijobt = "[WeBT]"; // Prefijo del nombre del dispositivo Bluetooth
+const String btname = "Web Viewer"; // Nombre del dispositivo Bluetooth
 
-String ssid = "&none/";  // SSID
-String password = "&none/";  // PASS
+String ssid = "";  // SSID
+String password = "";  // PASS
 String guardar_red = "false";  // Guardar red
 String web_ip = "none";  // IP
 String estado_web = "false";
 
-BluetoothSerial SerialBT;
-int conexion_bt = 0;
-
 Preferences preferences; // Crea un objeto de almacenamiento
 
+BluetoothSerial SerialBT;
+int conexion_bt = 0; // Proceso de conexion Bluetooth Classic
+
+WebServer server(80); // Crea un servidor web en el puerto 80
+
+// Funciones
 void bt_begin();
 void bluetooth();
 void wifi_begin(String ssid, String password);
 void handleRoot();
 
-WebServer server(80);
+/*========================================================================================================*/
 
 void setup() {
   // Iniciar la comunicación serial para monitoreo
   Serial.begin(115200);
 
+  // Iniciar el almacenamiento de preferencias
   preferences.begin("config_wifi", true); // Iniciar el almacenamiento
-  ssid = preferences.getString("ssid", "&none/"); // Leer el SSID
-  password = preferences.getString("password", "&none/"); // Leer la contraseña
+  ssid = preferences.getString("ssid", ""); // Leer el SSID
+  password = preferences.getString("password", ""); // Leer password
   preferences.end(); // Finalizar el almacenamiento
+  
+  // Iniciar el Wi-Fi, si hay datos guardados
+  if ((ssid != "") && (password != "")) wifi_begin(ssid, password);
 
-  if ((ssid != "&none/") && (password != "&none/")) wifi_begin(ssid, password);
-
+  // Iniciar Bluetooth Classic
   bt_begin();
 }
 
@@ -46,24 +61,26 @@ void loop() {
   delay(2500);
 }
 
+/*========================================================================================================*/
+
 void bt_begin() {
   SerialBT.end();
   delay(2000);
-  // Iniciar el Bluetooth
-  // [ESP] es el prefijo que se mostrará en el nombre del dispositivo
-  // Si el prefijo se cambia, también se debe cambiar en la aplicación móvil
-  if (!SerialBT.begin("[ESP] " + btname)) Serial.println("¡No se pudo iniciar el Bluetooth!");
-  else Serial.println("El Bluetooth está encendido y esperando conexión...");
+
+  if (!SerialBT.begin(prefijobt + " " + btname)) Serial.println("No se pudo iniciar el Bluetooth...");
+  else Serial.println("Dispositivo Bluetooth iniciado...");
 }
 
+// Funcion Bluetooth
 void bluetooth() {
   if (SerialBT.connected()) {
-    // Si la conexión es exitosa, enviar peticion
+    // Si la conexion es exitosa, enviar peticion
     if (conexion_bt == 0) {
       SerialBT.println("&ESP.PASS&");
       conexion_bt = 1;
     }
-    // Si no recibe la peticion correcta, reiniciar la conexión
+
+    // Si no recibe la peticion correcta, reiniciar la conexion
     else if (conexion_bt == 1) {
       if (SerialBT.available()) {
         String receivedData = SerialBT.readString();
@@ -77,17 +94,21 @@ void bluetooth() {
         } 
       }
     }
-    // Conexión exitosa, enviar datos
-    else if (conexion_bt == 2) {
-      if (estado_web == "true") SerialBT.println("/" + ssid + "&" + web_ip + "&true#");
-      else if (estado_web == "false") SerialBT.println("/ESP.OK&IP&false#");
 
+    // Conexion exitosa, enviar datos
+    else if (conexion_bt == 2) {
+      if (estado_web == "true") SerialBT.println("/" + ssid + "&" + web_ip + "&true&error#"); // Envio de datos al dispositivo movil
+      else if (estado_web == "false") SerialBT.println("/ESP.OK&IP&false&error#"); // Si el estado de la web es falso, enviar un mensaje de "simple" al dispositivo movil
+      
+      // Si hay datos disponibles, recibir y procesar
       if (SerialBT.available()) {
         String msg = SerialBT.readString();
         
         // Filtrar y separar los datos recibidos
-        if ((msg[0] != '%') && (msg[msg.length()-1] != '%')) Serial.println("Datos BT descartados...");
+        if ((msg[0] != '%') || (msg[msg.length()-1] != '%')) Serial.println("Datos BT descartados...");
         else {
+          estado_web = "false";
+          // Separar los datos recibidos
           Serial.println("Datos: " + msg);
           String separacion_msg[4];
           int x = 0;
@@ -96,20 +117,27 @@ void bluetooth() {
             else if (msg[i] == '&') x++;
             else separacion_msg[x] = separacion_msg[x] + msg[i];
           }
+
+          // Asignar los datos recibidos
           ssid = separacion_msg[1];
           password = separacion_msg[2];
           guardar_red = separacion_msg[3];
+
+          // Guardar los datos en la memoria
           preferences.begin("config_wifi", false); // Iniciar el almacenamiento
           if (guardar_red == "true") {
             Serial.println("Guardando red en memoria...");
             preferences.putString("ssid", ssid); // Guardar el SSID
-            preferences.putString("password", password); // Guardar la contraseña
+            preferences.putString("password", password); // Guardar la password
           }
           else {
+            // Limpiar la memoria, si no se desea guardar la red
             Serial.println("Borrando red de memoria...");
             preferences.clear(); // Limpiar el almacenamiento
           }
           preferences.end(); // Finalizar el almacenamiento
+          
+          // Conectar a la red WiFi
           wifi_begin(ssid, password);
         }
       }
@@ -117,10 +145,11 @@ void bluetooth() {
   }
   else {
     conexion_bt = 0;
-    Serial.println("Esperando reconexión...");
+    Serial.println("Esperando reconexion...");
   }
 }
 
+// Iniciar Wi-Fi
 void wifi_begin(String ssid, String password) {
   // Desconectar de la red Wi-Fi actual
   WiFi.disconnect();
@@ -129,23 +158,32 @@ void wifi_begin(String ssid, String password) {
   // Conectar a la red Wi-Fi
   WiFi.begin(ssid.c_str(), password.c_str());
   Serial.print("Conectando a WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+
+  // Tiempo de espera
+  int intentos = 0;
+  int max_intentos = 5;
+  while (WiFi.status() != WL_CONNECTED && intentos < max_intentos) {
+      delay(500);
+      Serial.print(".");
+      intentos++;
   }
   Serial.println("");
-  
-  // Conexión exitosa, guardar red
+
+  if (WiFi.status() != WL_CONNECTED) {
+      // Si no se conecta despues de los intentos, mostrar un mensaje de error
+      // ERROR 503: No se pudo conectar
+      estado_web = "false";
+      SerialBT.println("/ESP.OK&IP&false&503#");
+      Serial.println("Error al conectar a WiFi...");
+      return; // Salir de la función
+  }
+
+  Serial.println("");
+
+  // Conexion exitosa, guardar IP
   Serial.print("Dirección IP: ");
   web_ip = WiFi.localIP().toString();
   Serial.println(web_ip);
-
-  // Configurar mDNS para usar 'esp32.local'
-  if (!MDNS.begin(mdnsname)) {
-    Serial.println("Error al iniciar mDNS");
-    while (1);
-  }
-  Serial.println("mDNS iniciado: esp32.local");
 
   // Configurar las rutas del servidor web
   server.on("/", HTTP_GET, handleRoot);
@@ -156,7 +194,7 @@ void wifi_begin(String ssid, String password) {
   estado_web = "true";
 }
 
-// Ejemplo de página HTML con formulario
+// Pagina web
 void handleRoot() {
   String html = "<html><head><title>Formulario ESP32</title>";
   html += "<style>";
